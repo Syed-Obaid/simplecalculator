f6613c7900602f7e5af8975e4ba76fa5131f60a6
Syed Obaid-ullah Sharif <117272972+Syed-Obaid@users.noreply.github.com>
2024-03-20 18:00:21 +0500

    daldiya

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter firstnumber", type: "number", name: "firstnumber" },
+    { message: "Enter Secondnumber", type: "number", name: "secondnumber" },
+    {
+        message: "Enter Operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "division"],
+    },
+]);
+// Conditional Statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Enter right operator");
+}
